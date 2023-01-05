9c04923101208de78a5a8ba8ef81f4adcd383360
ubukawa <ubukawa-t96dq@mlit.go.jp>
2023-01-05 14:23:51 -0500

    rev

---
+++ index-s.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const data = JSON.parse(fs.readFileSync('sentinel2-grid.geojson', 'utf-8'))
+const output = {}
+
+//Geojson
+output.type = "FeatureCollection"
+output.features = []
+
+for (const i in data.features){
+    //console.log(data.features[i].properties.name2)
+    output.features[i] = {}
+    output.features[i].type = "Feature"
+    output.features[i].properties = {}
+    output.features[i].properties.name = data.features[i].properties.name2
+    output.features[i].geometry = {}
+    output.features[i].geometry.type = "Polygon"
+    output.features[i].geometry.coordinates = data.features[i].geometry.geometries[0].coordinates
+//    console.log(f.properties.name2)
+}
+
+
+
+fs.writeFileSync('./output-s.geojson', JSON.stringify(output))
